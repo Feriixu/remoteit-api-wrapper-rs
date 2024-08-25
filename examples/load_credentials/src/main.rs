853dd6e0226d9abdce0492f8201e2197db827265
Feriixu/Feriixu <>
2024-08-25 02:24:07 +0200

    chore: add example for loading credentials

---
+++ examples/load_credentials/src/main.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+use remoteit_api::credentials::Credentials;
+use remoteit_api::R3Client;
+
+fn main() {
+    // You can build the credentials manually like this.
+    let credentials = Credentials::builder()
+        .r3_access_key_id("access_key_id")
+        .r3_secret_access_key("secret_access_key")
+        .build();
+
+    // If the `credentials_loader` feature is enabled, the `load_from_disk` method is available.
+    // By default (and as per remote.it spec), the credentials are loaded from `~/.remoteit/credentials`.
+    let credentials = Credentials::load_from_disk().call().unwrap();
+
+    // Or you can load the credentials from a custom file path, if you prefer.
+    let mut credentials = Credentials::load_from_disk()
+        .custom_credentials_path("../../.env.remoteit") // This can be a &str, or a PathBuf, or anything that implements Into<PathBuf>.
+        .call()
+        .unwrap();
+
+    // Choosing a profile from the loaded credentials file.
+    // Pro Tip: Use `remove` to avoid having to clone.
+    let default_profile = credentials.remove("default").unwrap();
+
+    // Once you have the credentials, you can build the client.
+    let client = R3Client::builder()
+        .credentials(default_profile)
+        .build();
+}
